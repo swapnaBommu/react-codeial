6d0c46dc850738d5fc513d25f96979ce877d1f52
swapnaBommu <swapnabommu8143@gmail.com>
2023-08-19 16:19:11 +0530

    implemented customfetch method and fetches posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
-
+import { useEffect } from "react";
+import {getPosts} from "../api/index";
 function App() {
+  useEffect(() =>{
+
+    const fetchPosts = async () =>{
+      const response = await getPosts();
+      console.log('response',response);
+    }
+    fetchPosts();
+  },[]);
   return (
     <div className="App">
      codeial
