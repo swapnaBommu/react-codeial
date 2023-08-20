019b7b9b57ffbca006ba805ae27a0f7c380b1e6f
swapnaBommu <swapnabommu8143@gmail.com>
2023-08-20 15:57:20 +0530

    fetching posts in home page and navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {getPosts} from "../api/index";
+import Loader from "./Loader";
+import Home from "../pages/home";
+import Navbar from "./Navbar";
+
 function App() {
+  const [posts,setPosts] = useState([]);
+  const [loading,setLoading] = useState(true);
   useEffect(() =>{
 
     const fetchPosts = async () =>{
       const response = await getPosts();
       console.log('response',response);
+      if(response.success){
+        setPosts(response.data.posts);
+      }
+      setLoading(false);
     }
     fetchPosts();
   },[]);
+
+  if(loading){
+    return <Loader/>
+  }
   return (
     <div className="App">
-     codeial
+      <Navbar/>
+     <Home posts={posts} />
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import App from './App';
+import Loader from './Loader';
+import Navbar from './Navbar';
+
+
+
+export {App, Loader,Navbar};
