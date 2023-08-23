ec0cd125a3fab6d355104b084cf98f48fce3d5cd
swapnaBommu <swapnabommu8143@gmail.com>
2023-08-23 21:32:14 +0530

    Forms hooks

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+import {ToastProvider} from 'react-toast-notifications';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-   
+    <ToastProvider autoDismiss={true} autoDismissTimeout={5000} placement="top-left">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ToastProvider>  
   </React.StrictMode>
 );
 
